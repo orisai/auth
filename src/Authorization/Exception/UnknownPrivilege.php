0c17ac0d444212b0edadfcd9d7cd66566484a1f9
Marek Bartoš <bartos.developer152@gmail.com>
2021-11-13 18:20:46 +0100

    UnknownPrivilege exception namespace, tests

---
+++ src/Authorization/Exception/UnknownPrivilege.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types = 1);
+
+namespace Orisai\Auth\Authorization\Exception;
+
+use Orisai\Exceptions\LogicalException;
+use Orisai\Exceptions\Message;
+
+final class UnknownPrivilege extends LogicalException
+{
+
+	private string $privilege;
+
+	private function __construct(string $privilege)
+	{
+		parent::__construct();
+		$this->privilege = $privilege;
+	}
+
+	/**
+	 * @param class-string $class
+	 */
+	public static function forFunction(string $privilege, string $class, string $function): self
+	{
+		$self = new self($privilege);
+
+		$message = Message::create()
+			->withContext("Trying to call $class->$function().")
+			->withProblem("Privilege '$privilege' is unknown.")
+			->withSolution('Add privilege to data builder first via addPrivilege().');
+		$self->withMessage($message);
+
+		return $self;
+	}
+
+	public function getPrivilege(): string
+	{
+		return $this->privilege;
+	}
+
+}
