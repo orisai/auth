c19f46af3c475b9a579c195a3d2e57e43862266d
Marek Bartoš <bartos.developer152@gmail.com>
2021-11-18 19:42:05 +0100

    SimpleFirewall

---
+++ tests/Unit/Authentication/SimpleFirewallTest.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Orisai\Auth\Unit\Authentication;
+
+use Orisai\Auth\Authentication\ArrayLoginStorage;
+use Orisai\Auth\Authentication\IntIdentity;
+use Orisai\Auth\Authentication\SimpleFirewall;
+use Orisai\Auth\Authorization\AuthorizationDataBuilder;
+use Orisai\Auth\Authorization\PrivilegeAuthorizer;
+use Orisai\Auth\Authorization\SimplePolicyManager;
+use PHPUnit\Framework\TestCase;
+use Tests\Orisai\Auth\Doubles\AlwaysPassIdentityRenewer;
+
+final class SimpleFirewallTest extends TestCase
+{
+
+	public function test(): void
+	{
+		$storage = new ArrayLoginStorage();
+		$firewall = new SimpleFirewall(
+			'simple',
+			$storage,
+			new AlwaysPassIdentityRenewer(),
+			new PrivilegeAuthorizer(
+				new SimplePolicyManager(),
+				(new AuthorizationDataBuilder())->build(),
+			),
+			null,
+		);
+
+		self::assertFalse($storage->alreadyExists('simple'));
+		$firewall->login(new IntIdentity(123, []));
+		self::assertTrue($storage->alreadyExists('simple'));
+	}
+
+}
